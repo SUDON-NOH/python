d94557f13cbe65c33e998f113daf7396a26ae21d
DONY NOH <51067105+SUDON-NOH@users.noreply.github.com>
2019-07-10 04:38:27 -0700

    Add files via upload

---
+++ mod1.py
@@ -0,0 +1,13 @@
+# mod1.py
+
+def add(a, b):
+    print("add is called")
+    return a + b
+
+def sub(a, b):
+    print("sub is called")
+    return a - b
+
+if __name__ == "__main__":
+    print(add(1, 4))
+    print(sub(4, 2))
