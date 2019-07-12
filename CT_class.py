56c773d4837415667877fe8a97bc91dcf7564034
DONY NOH <51067105+SUDON-NOH@users.noreply.github.com>
2019-07-11 19:35:29 -0700

    계산기(1)

---
+++ CT_class.py
@@ -0,0 +1,20 @@
+# # 클래스 활용 기본 실습 문제
+#   1 (1)
+
+class Calculator():
+
+    def __init__(self):
+        print('Calculator 생성자 호출')
+        self.operator = {1:'+', 2:'-', 3:'*', 4:'/'}
+        self.a = 0
+        self.b = 0
+        self.op = 0
+
+    def calculate(self, a, b, op):
+        self.op = self.operator[op]
+        return eval('a' + self.op + 'b')
+
+
+if __name__ == '__main__':
+    sec = Calculator()
+    print(sec.calculate(10, 5, 2))
